eac846199fe54452f1fc9671363b9dc10d808302
momon-stars <driftmax@outlook.jp>
2025-05-15 23:47:18 +0900

    再修正

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -1,23 +1,18 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
 //for文の場合
-for (let i=0;i < holidays.length;i ++ ) {
-console.log(holidays[i]);
-}
+// for (let i=0;i < holidays.length;i ++ ) {
+// console.log(holidays[i]);
+// }
 
 // while文の場合
 const holidaysLength = holidays.length;
 const totalLoops = 1;
-const totalElements = holidaysLength * totalLoops;
+const totalElements = holidays.length;
 let i = 0;
-let count = 0;
 
-while ( count < totalElements) {
+while ( i < totalElements) {
   console.log(holidays[i]);
   i ++;
-  if( i === holidaysLength) {
-    i = 0;
-  }
-  count ++ ;
 }
 
