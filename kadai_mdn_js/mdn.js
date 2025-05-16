f63c5203076fd35349e858edf774d14cd2af0caf
momon-stars <driftmax@outlook.jp>
2025-05-16 09:57:22 +0900

    修正フォルダ、アップロード

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,14 @@
-const todayDate = new Date('2025-06-17');
+// const todayDate = new Date('2025-06-17');
 
 
-console.log(todayDate.getUTCFullYear()+"年"+todayDate.getUTCMonth()+"月"+todayDate.getUTCDate()+"日");
+// console.log(todayDate.getUTCFullYear()+"年"+todayDate.getUTCMonth()+"月"+todayDate.getUTCDate()+"日");
+
+const today = new Date();
+const year = today.getFullYear();
+const todayMoon = new Date();
+const moon = todayMoon.getMonth()+1;
+const todayDay =new Date();
+const day = todayDay.getDate();
+// const day = todayDay.getDay(); 
+// console.log(year+"年"+moon+"月"+day+"日");
+console.log(year+"年"+moon+"時"+day+"日");
